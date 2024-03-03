bfec716cdc3d366c8a6aa86c8eeaa36d0ea14013
NameIsNick <ysu0219@gmail.com>
2024-03-03 23:46:15 +0900

    SET:: global style 준비

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import GlobalStyle from "./styles/global";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <div style={{ fontSize: "10rem" }}>안녕하세요</div>
       </header>
     </div>
   );
---
+++ src/styles/global.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 10px;
+  }
+`;
+
+export default GlobalStyle;
