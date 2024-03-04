79ce4e49854f88983026b2729df069a7e697a69c
NameIsNick <ysu0219@gmail.com>
2024-03-04 13:32:45 +0900

    SET:: react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<></>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
