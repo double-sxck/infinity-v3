5dac0b2daa4354874b5fa136102146e8a2ea8714
NameIsNick <ysu0219@gmail.com>
2024-03-05 16:53:46 +0900

    SET:: global flex box

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -1,3 +1,10 @@
 import styled from "styled-components";
 
-export const Header = styled.header``;
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 8rem;
+  background-color: yellow;
+`;
---
+++ src/styles/theme/global.tsx
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 10px;
+  }
+
+  a {
+    text-decoration: none;
+    color: white;
+  }
+
+  input {
+    outline: none;
+  }
+
+  button {
+    outline: none;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+export default GlobalStyle;
