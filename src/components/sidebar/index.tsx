2b53ba17ef9a15bbfe89fb9d56ad3f2d5bf1c50f
NameIsNick <ysu0219@gmail.com>
2024-03-06 20:21:32 +0900

    ADD:: publising sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -5,66 +5,64 @@ import {
   HeartArrowIcon,
   HeartIcon,
   HomeIcon,
+  LogoIcon,
   MagicStickIcon,
   MoreIcon,
   PencilIcon,
   ScaryFaceIcon,
+  SmartLogoIcon,
   UserIcon,
 } from "../../assets";
-import { Culumn, Row } from "../../styles/ui";
-import Column from "../../styles/ui/flex/column";
+import { ItemBox, Row, Column } from "../../styles/ui";
 import * as S from "./style";
 import React from "react";
+
+const userIconComponents = [
+  { icon: <UserIcon />, text: "내 정보" },
+  { icon: <BookIcon />, text: "내 소설" },
+  { icon: <HeartIcon />, text: "좋아요한 소설" },
+  { icon: <PencilIcon />, text: "새 소설 쓰기" },
+];
+
+const genreIconComponents = [
+  { icon: <HeartArrowIcon />, text: "로맨스 / 감성" },
+  { icon: <MagicStickIcon />, text: "판타지 / 이세계" },
+  { icon: <CoffeeIcon />, text: "일상 / 코미디" },
+  { icon: <ScaryFaceIcon />, text: "스릴러 / 호러" },
+  { icon: <CopyIcon />, text: "장편 / 시리즈" },
+  { icon: <MoreIcon />, text: "그 외" },
+];
 
 const Sidebar = () => {
   return (
     <S.SidebarBox>
-      <Column>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <HomeIcon />
-          <S.SidebarText>홈</S.SidebarText>
+      <S.Between>
+        <Column gap={2.6}>
+          <ItemBox>
+            <HomeIcon />
+            <S.SidebarText>홈</S.SidebarText>
+          </ItemBox>
+          <S.SidebarContour />
+          {userIconComponents.map((item, index) => (
+            <ItemBox key={index}>
+              {item.icon}
+              <S.SidebarText>{item.text}</S.SidebarText>
+            </ItemBox>
+          ))}
+          <S.SidebarContour />
+          {genreIconComponents.map((item, index) => (
+            <ItemBox key={index}>
+              {item.icon}
+              <S.SidebarText>{item.text}</S.SidebarText>
+            </ItemBox>
+          ))}
+        </Column>
+        <Row gap={2} justifyContent="center" alignItems="center">
+          <SmartLogoIcon />
+          <S.SidebarVerticalLine />
+          <LogoIcon />
         </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <UserIcon />
-          <S.SidebarText>사용자</S.SidebarText>
-        </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <BookIcon />
-          <S.SidebarText>책</S.SidebarText>
-        </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <HeartIcon />
-          <S.SidebarText>하트</S.SidebarText>
-        </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <PencilIcon />
-          <S.SidebarText>연필</S.SidebarText>
-        </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <HeartArrowIcon />
-          <S.SidebarText>하트 화살표</S.SidebarText>
-        </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <MagicStickIcon />
-          <S.SidebarText>마술봉</S.SidebarText>
-        </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <CoffeeIcon />
-          <S.SidebarText>커피</S.SidebarText>
-        </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <ScaryFaceIcon />
-          <S.SidebarText>무서운 얼굴</S.SidebarText>
-        </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <CopyIcon />
-          <S.SidebarText>복사</S.SidebarText>
-        </Row>
-        <Row justifyContent="start" alignItems="center" gap={2.5}>
-          <MoreIcon />
-          <S.SidebarText>더보기</S.SidebarText>
-        </Row>
-      </Column>
+      </S.Between>
     </S.SidebarBox>
   );
 };
--- src/components/sidebar/style.ts
+++ src/components/sidebar/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const SidebarBox = styled.div`
   width: 24rem;
-  height: 100vh;
+  height: calc(100vh - 8rem);
 
   box-sizing: border-box;
   padding: 2.4rem;
@@ -14,4 +14,22 @@ export const SidebarBox = styled.div`
 export const SidebarText = styled.p`
   font-size: 2rem;
   font-weight: 400;
+`;
+
+export const SidebarContour = styled.div`
+  width: 100%;
+  border-top: 1px solid #9a9a9a;
+`;
+
+export const SidebarVerticalLine = styled.div`
+  height: 6.6rem;
+  border-left: 0.1rem solid black;
+`;
+
+export const Between = styled.div`
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
