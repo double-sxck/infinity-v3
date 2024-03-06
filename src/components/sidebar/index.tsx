e13d6facd248185b5a1200971c2e1c18cd2a551a
NameIsNick <ysu0219@gmail.com>
2024-03-06 17:59:09 +0900

    ADD:: 아이콘 글(refactoring 필요)

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -11,7 +11,7 @@ import {
   ScaryFaceIcon,
   UserIcon,
 } from "../../assets";
-import { Culumn } from "../../styles/ui";
+import { Culumn, Row } from "../../styles/ui";
 import Column from "../../styles/ui/flex/column";
 import * as S from "./style";
 import React from "react";
@@ -20,17 +20,50 @@ const Sidebar = () => {
   return (
     <S.SidebarBox>
       <Column>
-        <HomeIcon />
-        <UserIcon />
-        <BookIcon />
-        <HeartIcon />
-        <PencilIcon />
-        <HeartArrowIcon />
-        <MagicStickIcon />
-        <CoffeeIcon />
-        <ScaryFaceIcon />
-        <CopyIcon />
-        <MoreIcon />
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <HomeIcon />
+          <S.SidebarText>홈</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <UserIcon />
+          <S.SidebarText>사용자</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <BookIcon />
+          <S.SidebarText>책</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <HeartIcon />
+          <S.SidebarText>하트</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <PencilIcon />
+          <S.SidebarText>연필</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <HeartArrowIcon />
+          <S.SidebarText>하트 화살표</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <MagicStickIcon />
+          <S.SidebarText>마술봉</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <CoffeeIcon />
+          <S.SidebarText>커피</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <ScaryFaceIcon />
+          <S.SidebarText>무서운 얼굴</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <CopyIcon />
+          <S.SidebarText>복사</S.SidebarText>
+        </Row>
+        <Row justifyContent="start" alignItems="center" gap={2.5}>
+          <MoreIcon />
+          <S.SidebarText>더보기</S.SidebarText>
+        </Row>
       </Column>
     </S.SidebarBox>
   );
--- src/components/sidebar/style.ts
+++ src/components/sidebar/style.ts
@@ -4,6 +4,14 @@ export const SidebarBox = styled.div`
   width: 24rem;
   height: 100vh;
 
+  box-sizing: border-box;
+  padding: 2.4rem;
+
   position: fixed;
   top: 8rem;
+`;
+
+export const SidebarText = styled.p`
+  font-size: 2rem;
+  font-weight: 400;
 `;
