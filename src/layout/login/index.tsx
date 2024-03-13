f9709ac88e683802d4150610a9ff35a35c86a30f
NameIsNick <ysu0219@gmail.com>
2024-03-13 20:35:55 +0900

    CHORE

--- src/layout/login/index.tsx
+++ src/layout/login/index.tsx
@@ -2,6 +2,8 @@ import React, { useRef } from "react";
 import * as S from "./style";
 import { useOutSideClick } from "../../hooks/useOutsideClick";
 import { useLoginModal } from "../../hooks/useLoginMdal";
+import { Column } from "../../styles/ui";
+import { LogoTextIcon } from "../../assets";
 
 const LoginModal = () => {
   const { closeModal } = useLoginModal();
@@ -11,7 +13,19 @@ const LoginModal = () => {
 
   return (
     <S.Page>
-      <S.Modal ref={ref}></S.Modal>
+      <S.Modal ref={ref}>
+        <Column gap={8} justifyContent="center" alignItems="center">
+          <div>
+            <LogoTextIcon color="#000000" width={240} height={66.45} />
+            <S.ModalMainText>로그인</S.ModalMainText>
+          </div>
+          <S.InputText type="text" placeholder="아이디" />
+          <S.Row>
+            <S.CreateText>계정 만들기</S.CreateText>
+            <S.NextButton type="button" value={"다음"} />
+          </S.Row>
+        </Column>
+      </S.Modal>
     </S.Page>
   );
 };
