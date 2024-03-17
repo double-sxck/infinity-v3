086fc20a1d460c0220e79eb99bf9b85171cc97d9
NameIsNick <ysu0219@gmail.com>
2024-03-18 00:17:20 +0900

    ADD:: styled components 설정

--- src/components/write/index.tsx
+++ src/components/write/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import * as S from "./style";
+import { Column, Row } from "../../styles/ui";
+import { UserIcon } from "../../assets";
 
 const WritePage = () => {
-  return <>쓰는곳</>;
+  return (
+    <>
+      <Row>
+        <Column>
+          <S.ContentText>소설 구조 설정</S.ContentText>
+          <S.ContentBox>
+            <Row alignItems="center">
+              <UserIcon width={2.4} height={2.4} />
+              <S.RowText>인물</S.RowText>
+              <S.ContentInputBox placeholder="인물추가..." type="text" />
+            </Row>
+          </S.ContentBox>
+        </Column>
+      </Row>
+    </>
+  );
 };
 
 export default WritePage;
--- src/components/write/style.ts
+++ src/components/write/style.ts
@@ -1,1 +1,36 @@
 import styled from "styled-components";
+
+export const ContentBox = styled.div`
+  width: 48rem;
+  height: 66.3rem;
+
+  border: 0.1rem solid #9a9a9a;
+  border-radius: 2.4rem;
+`;
+
+export const ContentText = styled.p`
+  font-size: 3.2rem;
+  font-weight: 400;
+  color: #2c333e;
+  text-align: center;
+`;
+
+export const ContentInputBox = styled.input`
+  width: 22rem;
+  height: 4.4rem;
+  border: 0.2rem solid #2c333e;
+  border-radius: 2.4rem 2.4rem 2.4rem 0rem;
+  padding: 0.5rem;
+  font-size: 2.4rem;
+  font-weight: 400;
+
+  ::placeholder {
+    color: #9a9a9a;
+  }
+`;
+
+export const RowText = styled.p`
+  color: #2c333e;
+  font-size: 2.4rem;
+  font-weight: 400;
+`;
