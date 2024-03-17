c6138bb2074065b74783de7cb2cfdfd987fb17b0
NameIsNick <ysu0219@gmail.com>
2024-03-18 02:34:10 +0900

    SET:: view page 셋팅

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,4 @@ export { default as Sidebar } from "./sidebar/index";
 export { default as MainPage } from "./main/index";
 export { default as UserPage } from "./userPage/index";
 export { default as WritePage } from "./write/index";
+export { default as ViewPage } from "./view/index";
---
+++ src/components/view/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const ViewPage = () => {
+  return <>view</>;
+};
+
+export default ViewPage;
