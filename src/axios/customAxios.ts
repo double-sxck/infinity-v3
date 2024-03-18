34cda59db41918f72fe9c2718a3c880b535752c6
NameIsNick <ysu0219@gmail.com>
2024-03-19 00:11:55 +0900

    SET:: customAxios

---
+++ src/axios/customAxios.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const CustomAxios = axios.create({
+  baseURL: "http://localhost:3001/api-docs/",
+  withCredentials: true,
+});
+
+CustomAxios.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+CustomAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error?.response?.data?.message);
+    if (error?.response?.status === 401) {
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default CustomAxios;
