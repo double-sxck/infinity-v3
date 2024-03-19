be56218f21da3601a258e69e95b1a29fc2919cec
NameIsNick <ysu0219@gmail.com>
2024-03-19 17:32:15 +0900

    CHORE:: 주석처리

--- src/apis/instance/index.ts
+++ src/apis/instance/index.ts
@@ -9,30 +9,30 @@ instance.interceptors.response.use(
     return res;
   },
   async (error) => {
-    if (
-      error.response.status === 403 &&
-      localStorage.getItem("refresh-token")
-    ) {
-      try {
-        axios
-          .put(
-            "http://findfriend.kro.kr/api/auth/refresh",
-            {},
-            {
-              headers: {
-                "Authorization-refresh": `Bearer ${localStorage.getItem(
-                  "refresh-token"
-                )}`,
-              },
-            }
-          )
-          .then(({ data }) => {
-            localStorage.setItem("access-token", data.accessToken);
-            localStorage.setItem("refresh-token", data.refreshToken);
-          });
-      } catch (e) {}
-    }
-
-    return Promise.reject(error);
+    console.log(error);
+    //     if ( // 403에러가 뜨고 토큰이 있을때 재발급해주는 코드
+    //       error.response.status === 403 &&
+    //       localStorage.getItem("refresh-token")
+    //     ) {
+    //       try {
+    //         axios
+    //           .put(
+    //             "http://localhost:3001/api/v3  ",
+    //             {},
+    //             {
+    //               headers: {
+    //                 "Authorization-refresh": `Bearer ${localStorage.getItem(
+    //                   "refresh-token"
+    //                 )}`,
+    //               },
+    //             }
+    //           )
+    //           .then(({ data }) => {
+    //             localStorage.setItem("access-token", data.accessToken);
+    //             localStorage.setItem("refresh-token", data.refreshToken);
+    //           });
+    //       } catch (e) {}
+    //     }
+    //     return Promise.reject(error);
   }
 );
