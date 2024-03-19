4e6fae8bce8b5151922c74e09125ba1855a5de2a
NameIsNick <ysu0219@gmail.com>
2024-03-20 00:48:34 +0900

    FIX:: userIcon rem 단위 수정

--- src/assets/images/UserIcon.tsx
+++ src/assets/images/UserIcon.tsx
@@ -5,7 +5,7 @@ interface SVGAttributeProps extends React.SVGAttributes<HTMLOrSVGElement> {
   height?: number;
 }
 
-const UserIcon = ({ width = 24, height = 24 }: SVGAttributeProps) => {
+const UserIcon = ({ width = 2.4, height = 2.4 }: SVGAttributeProps) => {
   return (
     <>
       <svg
