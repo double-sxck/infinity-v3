19491ede6ac442dc5b7df270c104850dcfdbe7bd
NameIsNick <ysu0219@gmail.com>
2024-03-20 17:13:41 +0900

    FIX:: git 꼬임 고치기

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
 } from "./components";
 import NovelSearchBox from "./components/main/search/index";
 import { useLoginModal } from "./hooks/useLoginMdal";
-import LoginModal from "./layout/LoginModal/modal";
+import LoginModal from "./layout/modal";
 
 function App() {
   const { modalState } = useLoginModal();
