c73b0b7bc381ab793bdb5e777ddb88fd0bfff82c
NameIsNick <ysu0219@gmail.com>
2024-03-21 00:18:58 +0900

    ADD:: useOutsideClick hook 추가

--- src/layout/CommentModal/index.tsx
+++ src/layout/CommentModal/index.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import * as S from "./style";
+import { useCommentModal } from "../../hooks/useCommentModal";
+import { useOutSideClick } from "../../hooks/useOutsideClick";
 
 const CommentModal = () => {
+  const { closeCommentModal } = useCommentModal();
+  const ref = useRef<HTMLDivElement>(null);
+  useOutSideClick(ref, closeCommentModal);
   return (
     <S.Page>
-      <S.Modal></S.Modal>
+      <S.Modal ref={ref}></S.Modal>
     </S.Page>
   );
 };
