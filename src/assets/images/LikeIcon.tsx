235231201a1b7f231c5abb7365e28b945a120d01
NameIsNick <ysu0219@gmail.com>
2024-03-21 23:55:22 +0900

    REFACTORING:: get novel

--- src/assets/images/LikeIcon.tsx
+++ src/assets/images/LikeIcon.tsx
@@ -3,9 +3,14 @@ import React from "react";
 interface SVGAttributeProps extends React.SVGAttributes<HTMLOrSVGElement> {
   width?: number;
   height?: number;
+  color?: string;
 }
 
-const LikeIcon = ({ width = 4.0, height = 4.0 }: SVGAttributeProps) => {
+const LikeIcon = ({
+  width = 4.0,
+  height = 4.0,
+  color = "#D9d9d9",
+}: SVGAttributeProps) => {
   return (
     <svg
       width={`${width}rem`}
@@ -16,7 +21,7 @@ const LikeIcon = ({ width = 4.0, height = 4.0 }: SVGAttributeProps) => {
     >
       <path
         d="M40 20C40 31.0457 31.0457 40 20 40C8.9543 40 0 31.0457 0 20C0 8.9543 8.9543 0 20 0C31.0457 0 40 8.9543 40 20Z"
-        fill="#D9D9D9"
+        fill={color}
       />
       <path
         d="M25.4999 9.91653C24.3739 9.93405 23.2724 10.2484 22.3067 10.8278C21.341 11.4072 20.5453 12.2312 19.9999 13.2165C19.4546 12.2312 18.6589 11.4072 17.6932 10.8278C16.7275 10.2484 15.626 9.93405 14.4999 9.91653C12.7049 9.99452 11.0136 10.7798 9.7957 12.1008C8.57778 13.4217 7.93217 15.1711 7.99992 16.9665C7.99992 21.5135 12.7859 26.4795 16.7999 29.8465C17.6961 30.5997 18.8293 31.0126 19.9999 31.0126C21.1706 31.0126 22.3037 30.5997 23.1999 29.8465C27.2139 26.4795 31.9999 21.5135 31.9999 16.9665C32.0677 15.1711 31.4221 13.4217 30.2041 12.1008C28.9862 10.7798 27.295 9.99452 25.4999 9.91653Z"
--- src/layout/CommentModal/index.tsx
+++ src/layout/CommentModal/index.tsx
@@ -3,24 +3,73 @@ import * as S from "./style";
 import { useCommentModal } from "../../hooks/useCommentModal";
 import { useOutSideClick } from "../../hooks/useOutsideClick";
 import { Column, Row } from "../../styles/ui";
-import { HeartArrowIcon, LikeIcon, CommentIcon } from "../../assets";
 import { useGetListQuery } from "./api";
 import { queryClient } from "../../App";
 import { instance } from "../../apis/instance";
 import { Authorization } from "../../apis/authorization";
+import NovelType from "./NovelCategory";
+import { CommentIcon, LikeIcon } from "../../assets";
+
+interface CommentType {
+  userResult: {
+    nickname: string;
+  };
+  novelResult: {
+    uid: number;
+    user_uid: number;
+    title: string;
+    content: string;
+    thumbnail: string;
+    category: string;
+    views: number;
+    novel_likes: { user_uid: number; novel_uid: number }[];
+    like?: boolean;
+    likeCount: number;
+  }[];
+}
 
 var id = 1;
 const CommentModal = () => {
   const { closeCommentModal, modalCState } = useCommentModal(id);
   const ref = useRef<HTMLDivElement>(null);
-  useOutSideClick(ref, closeCommentModal);
+  useOutSideClick(ref, () => {
+    console.log(id, "열렸다잉");
+    // getComment();
+    closeCommentModal();
+  });
+
+  const [message, setMessage] = useState();
 
   //feed list 받아온 값을 저장하는 state
-  const [comment, setComment] = useState([]);
+  const [comment, setComment] = useState<CommentType[]>([
+    {
+      userResult: {
+        nickname: "aaaa",
+      },
+      novelResult: [
+        {
+          uid: 1,
+          user_uid: 4,
+          title: "새로운 포스트 제목",
+          content: "포스트 내용",
+          thumbnail: "이미지 URL 또는 경로",
+          category: "ETC",
+          views: 1034,
+          novel_likes: [
+            {
+              user_uid: 4,
+              novel_uid: 1,
+            },
+          ],
+          likeCount: 1,
+        },
+      ],
+    },
+  ]);
   // query key를 지정하여 새로고침 없이 실행시킬 react query
   const { data } = useGetListQuery(id) as { data: any };
   useLayoutEffect(() => {
-    setComment(data);
+    setMessage(data);
   }, [data]);
 
   useLayoutEffect(() => {
@@ -28,6 +77,7 @@ const CommentModal = () => {
   }, []);
 
   const getComment = async () => {
+    console.log("실행");
     try {
       id = modalCState.id;
       console.log("아이디" + id);
@@ -40,7 +90,7 @@ const CommentModal = () => {
   };
 
   const sendMessage = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.keyCode === 13) {
+    if (event.key === "enter") {
       // Enter 키를 눌렀는지 확인
       queryClient.invalidateQueries({ queryKey: ["getList", id - 1] });
     }
@@ -54,40 +104,45 @@ const CommentModal = () => {
             <Row gap={8.6}>
               <S.ImageBox />
               <Column gap={4}>
-                <S.NovelTitle>소설 제목</S.NovelTitle>
-                <S.NovelContent>마동현우</S.NovelContent>
+                <S.NovelTitle>{comment[0].novelResult[0].title}</S.NovelTitle>
+                <S.NovelContent>
+                  {comment[0].userResult.nickname}
+                </S.NovelContent>
                 <Row gap={3}>
-                  <HeartArrowIcon />
-                  <S.NovelContent>로멘스/감성</S.NovelContent>
+                  {comment[0]?.novelResult[0].category ? (
+                    <>
+                      {NovelType[comment[0].novelResult[0].category].icon}
+                      <S.NovelContent>
+                        {NovelType[comment[0].novelResult[0].category].label}
+                      </S.NovelContent>
+                    </>
+                  ) : (
+                    <>
+                      {NovelType.ETC.icon}
+                      <S.NovelContent>{NovelType.ETC.label}</S.NovelContent>
+                    </>
+                  )}
                 </Row>
-                <S.NovelContent>조회수 [n]</S.NovelContent>
+
+                <S.NovelContent>
+                  조회수 {comment[0].novelResult[0].views}
+                </S.NovelContent>
                 <Row>
-                  <LikeIcon />
-                  <S.NovelContent>[n]</S.NovelContent>
+                  {comment[0].novelResult[0].like ? (
+                    <LikeIcon color={"#ff0000"} />
+                  ) : (
+                    <LikeIcon />
+                  )}
+                  <S.NovelContent>
+                    {comment[0].novelResult[0].uid}
+                  </S.NovelContent>
                 </Row>
               </Column>
             </Row>
           </div>
           <S.HelfLine />
 
-          <S.NovelContent>
-            {" "}
-            [소설 내용] [가나다 라마 바사아 자차 카타파 하가 나다라마 바사아
-            자차 카타파하 가나다 라마 바사 아자차 카타 파하 가나 다 라마바
-            사아자차카타 파하가 나다 라 마바 사아자차카 타 파하 가나다 라마바
-            사아자 차 카타파 하가나 다라마바사 아자 차카타파 하가 나다 라 마바
-            사아 자 차카타파 하 가나 다 라마바사 아자 차 카타 파하 가 나 다라
-            마바사 아자 차카 타 파하 가 나다 라 마 바 사아 자차카 타파 하가나
-            다라 마바사 아 자 차 카타파 하 가나 다 라 마바 사아자차카 타파하
-            가나 다라 마 바사아자 차카타파 하가나다 라마바 사아자차카 타파하가
-            나 다라마 바사 아 자 차카 타 파 하가나다 라마 바사 아자차 카타 파하
-            가나 다 라 마바 사 아자차 카타 파하가나 다라 마바 사아 자차카타
-            파하가나 다라마 바사 아자차 카타파 하가 나다라마바사 아자차카타파
-            하가 나 다라 마바사아자차카 타파하가 나다라 마 바 사 아자차카타파
-            하가나 다라 마바 사아 자차 카 타파 하가나다라 마바사 아자차 카타파
-            하가 나다 라마 바사아 자차 카타파 하가 나다 라마 바사 아자 차카타
-            파하]
-          </S.NovelContent>
+          <S.NovelContent>{comment[0].novelResult[0].content}</S.NovelContent>
           <S.HelfLine />
           <div
             style={{
@@ -106,9 +161,7 @@ const CommentModal = () => {
             <div className="mb-20">
               <S.MessageInput
                 placeholder="감상평 남기기..."
-                // onChange={(e) => {
-                //   sendMessage(e);
-                // }}
+                onChange={(e) => {}}
                 onKeyDown={(e) => sendMessage(e)}
               />
             </div>
@@ -123,6 +176,3 @@ const CommentModal = () => {
 };
 
 export default CommentModal;
-function aysnc() {
-  throw new Error("Function not implemented.");
-}
