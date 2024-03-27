7d1f3032662c0c7d000e095e11cabcdb905f4f13
minseok0415 <kmskorea14@gmail.com>
2024-03-27 11:42:55 +0900

    CHORE:: 로컬스토리지 관리

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -60,11 +60,8 @@ const Sidebar = () => {
           <S.SidebarContour />
           <S.Subtitle>탐색</S.Subtitle>
           {GenreIconComponents.map((item, index) => {
-            localStorage.setItem("findType", item.type);
             return (
-              // Sidebar 컴포넌트의 코드
-
-              <Link to={`/find/${item.type}`} key={index+100}>
+              <Link to={`/find/${item.type}`} key={index}>
                 <ItemBox>
                   {item.icon}
                   <S.SidebarText>{item.text}</S.SidebarText>
